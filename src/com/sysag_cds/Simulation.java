799b56015fd80a9e6f26022c74d332c818e88210
Giuseppe Antonio Nanna <g.a.nanna@inventati.org>
2020-05-02 04:12:02 +0200

    L'agente Simulation avvia gli agenti

--- src/com/sysag_cds/Simulation.java
+++ src/com/sysag_cds/Simulation.java
@@ -1,5 +1,49 @@
 package com.sysag_cds;
 
-public class Simulation {
-    static int tick = 1000;
+import com.google.common.base.Supplier;
+import edu.uci.ics.jung.algorithms.generators.Lattice2DGenerator;
+import edu.uci.ics.jung.graph.*;
+import jade.core.Agent;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+public class Simulation extends Agent {
+    public static int tick = 1000;
+    public static boolean debug = true;
+
+    protected void setup()  {
+        ContainerController c = getContainerController();
+        AgentController a;
+
+        Object [] args = new Object[1];
+        args[0] = "infectious";
+
+        try {
+
+            a = c.createNewAgent( "contagioso", "com.sysag_cds.Person", args);
+            a.start();
+
+            Thread.sleep(1000);
+
+            a = c.createNewAgent( "sano", "com.sysag_cds.Person", null);
+            a.start();
+
+        } catch (StaleProxyException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Graph<Building,Road> map = (
+                new Lattice2DGenerator<Building, Road>(UndirectedSparseGraph.<Building, Road>getFactory(),
+                        new BuildingFactory(),
+                        new RoadFactory(),
+                        10, false
+                )
+        ).get();
+
+        System.out.println(map.toString());
+    }
 }
