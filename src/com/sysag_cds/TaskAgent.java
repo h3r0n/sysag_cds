0082148f54b807962118348670c850c83686c218
Giuseppe Antonio Nanna <g.a.nanna@inventati.org>
2020-05-17 13:03:29 +0200

    Definite classi astratte Task e TaskAgent
    per lo scheduling sequenziale dei behaviour

---
+++ src/com/sysag_cds/TaskAgent.java
@@ -0,0 +1,45 @@
+package com.sysag_cds;
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+abstract public class TaskAgent extends Agent {
+    Queue<Task> todo = new LinkedList<>(); // coda task non ancora eseguiti
+
+    abstract class Task extends Behaviour {
+
+        public Task() {
+            super();
+        }
+
+        public Task(Agent a) {
+            super(a);
+        }
+
+        @Override
+        public int onEnd() {
+            // il task in esecuzione è il primo della lista: rimuovilo perché è terminato
+            if (this == todo.peek())
+                todo.remove();
+
+            // esegui il prossimo task, senza rimuoverlo dalla lista (verrà rimosso quando terminerà)
+            if (todo.peek() != null)
+                addBehaviour(todo.peek());
+            return super.onEnd();
+        }
+    }
+
+    public void scheduleTask(Task t) {
+        // se la coda è vuota, non ci sono task in esecuzione, quindi il nuovo task dovrà essere eseguito manualmente
+        if (todo.peek() == null) {
+            todo.add(t);
+            addBehaviour(todo.peek());
+        // se la coda è piena, c'è un task in esecuzione, il quale eseguirà automaticamente il successivo alla terminazione
+        } else {
+            todo.add(t);
+        }
+    }
+}
